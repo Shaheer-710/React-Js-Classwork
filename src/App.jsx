83e468e1ac58901642c2b3c737233f3f075ddb95
Muhammad Shaheer <shaheer7w@gmail.com>
2024-12-22 20:50:50 +0500

    React Class 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,42 @@
 import React from 'react'
+import { data } from './components/data'
 import Cards from './components/Cards/Cards'
-import { Data } from './Data'
 
 const App = () => {
+
+  const myArray = [
+    "apple",
+    "banana",
+    "cherry",
+    "date",
+    "elderberry",
+    "fig",
+    "grape",
+    "honeydew",
+    "kiwi",
+    "lemon"
+  ];
+
   return (
     <>
-   
-
-    {Data.map(e,i => {
-
-      return  <Cards  key={i} imageUrl={e.imageUrl} description={e.description} />
-    })}
-
+      { 
+        data.map((e,i) => {
+          return(
+        <Cards  key={i} id={e.id} imageUrl={e.imageUrl} description={e.description} />  )        
+        })
+}       
+<select style={{
+  width:455,
+  padding:13,
+  backgroundColor:'black',
+  color:'white'
+}}>
+{
+  myArray.map((e,i)=>{
+    return  <option key={i}>{e}</option>
+  })
+}
+</select>
     </>
   )
 }
--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,15 +1,23 @@
 import React from 'react'
-
-const Cards = ({imageUrl, description }) => {
+import PropTypes from 'prop-types'
+const Cards = ({id,imageUrl, description }) => {
       return (
     <>
       <div className='cardstyle'  style={{width:500}}>
       <img src={imageUrl} alt="" />
+       <h2>Id : {id}</h2>
        <p>{description}</p>
    
 </div>
     </>
   )
 }
+
+Cards.propTypes ={
+    id:PropTypes.number,
+    h2:PropTypes.string,
+    p:PropTypes.string
+}
+
 
 export default Cards
